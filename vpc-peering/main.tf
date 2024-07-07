0d7b8babdb5451e97a609555ec449919c5810401
Ashok <ashokjeevan@gmail.com>
2024-07-07 18:01:01 -0400

    condition update for vpc peering resource

--- vpc-peering/main.tf
+++ vpc-peering/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "peering_connection" {
-  count = var.vpc_peering_enabled == false ? 0 : 1
+  count = var.vpc_peering_enabled == true ? 1 : 0
   peer_owner_id = var.aws_account_id # data.aws_caller_identity.current.id # target peer VPC owner id
-  peer_vpc_id   = var.target_vpc_id  # target peer VPC id
-  vpc_id        = var.requestor_vpc_id # requestor VPC id
+  peer_vpc_id   = var.target_vpc_id # module.vpc_a.vpc_id                 # target peer VPC id
+  vpc_id        = var.requestor_vpc_id #module.vpc_b.vpc_id                 # requestor VPC id
   auto_accept   = var.peering_connection_auto_accept
 }
