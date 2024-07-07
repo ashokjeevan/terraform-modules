63525352347cd34ab3fc3c05088ed5afcf106905
Ashok <ashokjeevan@gmail.com>
2024-07-07 17:43:12 -0400

    vpc peering connection output attribute addition

--- vpc-peering/outputs.tf
+++ vpc-peering/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_peering_connection_id" {
     value = aws_vpc_peering_connection.peering_connection.id
 }
+
+output "peering_connection_accept_status" {
+    value = aws_vpc_peering_connection.peering_connection.accept_status
+}
