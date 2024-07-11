bd33b3643dc0f0fa6fe938457ad113720022bbed
Ashok <ashokjeevan@gmail.com>
2024-07-10 20:16:36 -0400

    rds module

---
+++ rds/main.tf
@@ -0,0 +1,20 @@
+resource "aws_db_subnet_group" "rds_subnet_grp" {
+  subnet_ids = var.private_subnet_ids
+}
+
+resource "aws_db_instance" "rds_db" {
+  allocated_storage      = var.allocated_storage
+  engine                 = var.rds_engine        
+  engine_version         = var.rds_engine_version
+  instance_class         = var.rds_instance_class
+  db_subnet_group_name   = var.db_subnet_group_name 
+  vpc_security_group_ids = var.vpc_security_group_ids
+  db_name                = var.rds_db_name
+  username               = var.rds_db_username
+  password               = var.rds_db_password
+  skip_final_snapshot    = var.skip_final_snapshot
+
+  lifecycle {
+    ignore_changes = [password]
+  }
+}
---
+++ rds/variables.tf
@@ -0,0 +1,44 @@
+variable "allocated_storage" {
+  type = number
+}
+
+variable "rds_engine" {
+  type = string
+}
+
+variable "rds_engine_version" {
+  type = string
+}
+
+variable "rds_instance_class" {
+  type = string
+}
+
+variable "db_subnet_group_name" {
+  type = string
+}
+
+variable "vpc_security_group_ids" {
+  type = list(string)
+}
+
+variable "rds_db_name" {
+  type = string
+}
+
+variable "rds_db_username" {
+  type = string
+}
+
+variable "rds_db_password" {
+  type = string
+}
+
+variable "skip_final_snapshot" {
+  type    = bool
+  default = true
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
