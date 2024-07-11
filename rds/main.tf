43c13b499444e72b48051eb68d54657e416b9c6b
Ashok <ashokjeevan@gmail.com>
2024-07-10 20:37:57 -0400

    rds module update

--- rds/main.tf
+++ rds/main.tf
@@ -7,12 +7,16 @@ resource "aws_db_instance" "rds_db" {
   engine                 = var.rds_engine        
   engine_version         = var.rds_engine_version
   instance_class         = var.rds_instance_class
-  db_subnet_group_name   = var.db_subnet_group_name 
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_grp.id
   vpc_security_group_ids = var.vpc_security_group_ids
   db_name                = var.rds_db_name
   username               = var.rds_db_username
   password               = var.rds_db_password
   skip_final_snapshot    = var.skip_final_snapshot
+
+  tags = {
+    Name = var.rds_db_name
+  }
 
   lifecycle {
     ignore_changes = [password]
